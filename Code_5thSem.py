2834aed0e3b1bf5577e7d0e85b871a60afa001fe
Aaryan Mehta <aaryanmta@gmail.com>
2022-11-18 22:54:07 +0530

    Final

--- Code_5thSem.py
+++ Code_5thSem.py
@@ -10,4 +10,4 @@ with sr.Microphone() as source:
         text = r.recognize_google(audio)
         print("You said: {}".format(text))
     except:
-        print("Sorry couldn't recognize your speech")
+        print("Sorry couldn't recognize your speech.")
