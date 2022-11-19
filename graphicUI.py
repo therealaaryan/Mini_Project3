3f5fce07d21b2ed20c5908957292659005d8432e
Aaryan Mehta <aaryanmta@gmail.com>
2022-11-19 16:32:23 +0530

    text xhange in myLabel2

--- graphicUI.py
+++ graphicUI.py
@@ -6,7 +6,7 @@ root.geometry("700x350")
 root.title("Speech to Text")
 
 try:
-    myLabel2 = Label(root, text="You said: {}".format(csem.text), font=('Times', 24)).grid(row=1, column=0)
+    myLabel2 = Label(root, text="{}".format(csem.text), font=('Times', 24)).grid(row=1, column=0)
 except:
     myLabel3 = Label(root, text="Couldn't recognize your speech!", font=('Times', 24)).grid(row=1, column=0)
 
