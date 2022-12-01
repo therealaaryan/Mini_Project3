c8ba3d3bbbe57e722a33acef972bb7cdb6dadfef
Aaryan Mehta <aaryanmta@gmail.com>
2022-12-02 00:47:03 +0530

    final changes

--- Code_5thSem.py
+++ Code_5thSem.py
@@ -3,9 +3,19 @@ import speech_recognition as sr
 r = sr.Recognizer()
 
 with sr.Microphone() as source:
+    print("Listening.....")
     audio = r.listen(source)
 
     try:
         text = r.recognize_google(audio)
+        if "curly bracket" in text:
+            text = text.replace("curly bracket","\n{}")
+        elif "circular bracket" in text:
+            text = text.replace("circular bracket","()")
+        elif "semi colon" in text:
+            text = text.replace("semi colon",";")
+        elif "colon" in text:
+            text = text.replace("colon",":")
+
     except:
         pass
